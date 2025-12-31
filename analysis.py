42ce15c1a070d2eaf368a17ead1f99acd4015c8a
Adekunbi Oderinu <derinoderinu@gmail.com>
2025-12-31 18:16:31 -0500

    Added whale size classification column

--- analysis.py
+++ analysis.py
@@ -8,3 +8,18 @@ df = pd.read_csv("data/whales.csv")
  
 print(df.head(7))
 print(df.info()) 
+
+def categories(weight):
+    if weight < 20000:
+        category = "Light" 
+    elif weight > 80000:
+        category = "Heavy"
+    else:
+        category = "Medium"
+    return category
+
+print(categories(1000))
+
+df["size_category"]=df["avg_weight_kg"].apply(categories)
+print(df)
+print(df["size_category"].value_counts())
