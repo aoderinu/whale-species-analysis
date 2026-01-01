e9181b21ce5c882f1678345adb3b9d5b70b964dc
Adekunbi Oderinu <derinoderinu@gmail.com>
2026-01-01 15:15:36 -0500

    Created a bar graph that visualizes the findings of my analysis

--- analysis.py
+++ analysis.py
@@ -26,4 +26,20 @@ df["size_category"] = df["avg_weight_kg"].apply(categories)
 print(df)
 
 # Count how many whale species are in each category using .value_counts()
-print(df["size_category"].value_counts())
+category_counts = df["size_category"].value_counts()
+print(category_counts)
+
+# Interpretation
+# The most common size cateogry is "medium"
+# This suggests that the majority of the whale species in this dataset have average weights
+# between 20,000 and 80,000 kg, relecting that dataset's focus on mid - to - large whales 
+# rather that the entire range of whale species.
+
+# Visualization 
+import matplotlib.pyplot as plt
+
+# Create a var chart that shows the weight categoreisa and the number of species in each category
+plt.bar(category_counts.index, category_counts.values)
+plt.xlabel("Categories")
+plt.ylabel("Counts")
+plt.show()
